2234365f450413af425afa5816f868a0204b4697
laevad <77134175+laevad@users.noreply.github.com>
2023-11-01 15:29:01 +0800

    Data

---
+++ Data/Contexts/InventoryContext.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Driver;
+using Play.Common.Data;
+using Play.Inventory.Entities;
+
+namespace Play.Inventory.Data.Contexts;
+
+public class InventoryContext : IItemContext<InventoryItem>
+{
+    public InventoryContext(IConfiguration configuration)
+    {
+        var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+        var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+        
+        Items = database.GetCollection<InventoryItem>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+        
+        // InventoryContextSeed.SeedData(Items);
+    }
+    
+    public IMongoCollection<InventoryItem> Items { get; }
+}
---
+++ Data/Seeds/InventoryContextSeed.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Driver;
+using Play.Inventory.Entities;
+
+namespace Play.Inventory.Data.Seeds;
+
+public class InventoryContextSeed
+{
+    // seed data
+    public static void SeedData(IMongoCollection<InventoryItem> items)
+    {
+        var existItem = items.Find(item => true).Any();
+        if (!existItem)
+        {
+            items.InsertManyAsync(GetPreconfiguredItems());
+        }
+    }
+
+    private static IEnumerable<InventoryItem> GetPreconfiguredItems()
+    {
+        return new List<InventoryItem>
+        {
+            
+        };
+    }
+}
